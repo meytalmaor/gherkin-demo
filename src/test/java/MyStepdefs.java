4dd2e61834131c02a303bb7d501a6e4d8403f34b
meytalmaor <maor.meytal@gmail.com>
2018-08-19 16:48:53 +0300

    Gherkin Demo Project first commit

---
+++ src/test/java/MyStepdefs.java
@@ -0,0 +1,21 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class MyStepdefs {
+    @Given("^Some requirements$")
+    public void someRequirements() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+    @When("^This happens$")
+    public void thisHappens() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+    @Then("^That happens$")
+    public void thatHappens() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+    }
+}
---
+++ src/test/resources/features/Test1-1041.feature
@@ -0,0 +1,7 @@
+#Auto generated Octane revision tag
+@TID1041REV0.2.0
+Feature:
+	Scenario: Gherkin Demo Scenario
+		Given Some requirements 
+		When This happens
+		Then That happens
