c15fb10412b60333677242fc5e6045a2f1b3e616
meytalmaor <maor.meytal@gmail.com>
2018-08-27 19:55:36 +0300

    Added another exception

--- src/test/java/MyStepdefs.java
+++ src/test/java/MyStepdefs.java
@@ -85,12 +85,12 @@ public class MyStepdefs {
     @Then("^I am navigated to the laptop details page$")
     public void iAmNavigatedToTheLaptopDetailsPage() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        throw new Exception("The laptop details page did not load properly");
+
     }
 
     @When("^I click on a laptop\\.$")
     public void iClickOnALaptop2() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        throw new Exception("The laptop details page did not load properly");
     }
 }
